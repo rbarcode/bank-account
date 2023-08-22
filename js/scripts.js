14ae4fd481e4ef069a4e371bf7fef17abba2f288
Richard Barbour <richyb07@gmail.com>
2023-08-22 16:47:48 -0700

    Add UI logic and html code.
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 
+// Business Logic
+
 function BankAccount(name, initialDeposit, currentBalance) {
   this.name = name;
   this.initialDeposit = parseInt(initialDeposit);
@@ -13,9 +15,26 @@ BankAccount.prototype.addDeposit = function(deposit) {
   this.currentBalance = this.currentBalance + parseInt(deposit);
   return this.currentBalance;
 };
-
+    // this prototype passes test #4
 BankAccount.prototype.subtractWithdrawl = function(withdrawl) {
   this.currentBalance = this.currentBalance - parseInt(withdrawl);
   return this.currentBalance;
 };
  
+
+// UI Logic
+
+window.addEventListener("load", submit);
+
+function submit() {
+  const form = document.getElementById("registration");
+  form.addEventListener("submit", handleFormSubmission);
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedName = document.getElementById("name").value;
+  const initialDeposit = document.getElementById("initial-deposit").value;
+  let acct = new BankAccount(inputtedName, initialDeposit);
+  console.log(acct);
+}
