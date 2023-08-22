a71723680b6cd1ac8bc4768111a8beb9dc0dce84
Richard Barbour <richyb07@gmail.com>
2023-08-22 15:41:15 -0700

    Pass test 2
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,5 @@ function BankAccount(name, initialDeposit, currentBalance) {
   this.name = name;
   this.initialDeposit = initialDeposit;
   this.currentBalance = currentBalance;
+  this.currentBalance = this.initialDeposit;
 }
